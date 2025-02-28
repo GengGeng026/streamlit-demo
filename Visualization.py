67550917ba485a4a0e561bcaae79eef1095a20a6
GengGeng026 <danielngkahking@gmail.com>
2025-02-28 15:04:34 +0800

    again

--- Visualization.py
+++ Visualization.py
@@ -58,7 +58,7 @@ st.markdown(
     /* 統一表格樣式 */
     .legend-table, .styled-table {
         width: 100%;
-        height: 10vh; /* 與圖表一致 */
+        height: 20vh; /* 與圖表一致 */
         border-collapse: separate;
         border-spacing: 0;
         border: 1px solid #444;
